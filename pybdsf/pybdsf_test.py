8ef7dcbf48561f615edca89f4a784c6dce4e43c5
dparodi935 <dparodi935@gmail.com>
2026-07-11 19:08:53 +0100

    Fixed PyBDSF test to avoid memory issues

--- pybdsf/pybdsf_test.py
+++ pybdsf/pybdsf_test.py
@@ -3,12 +3,14 @@ import os
 
 #script_dir = os.path.dirname(os.path.abspath(__file__))
 fits_path = os.path.join('..','lofar_downloads')
-fits_file = 'facet_27'
+fits_file = 'facet_19'
 
 #creates catalog. thresh_pix and isl are the S/N ratios for the peaks and surrounding island respectively.
-img = bdsf.process_image(os.path.join(fits_path,f'{fits_file}.fits'), thresh_pix=5.0, thresh_isl=4.0)
+img = bdsf.process_image(os.path.join(fits_path,f'{fits_file}.fits'), thresh_pix=5.0, thresh_isl=4.0, ncores=2)
+
+
 #adaptive rms box means the box used to measure noise shrinks near bright sources to capture the noise artefacts
-img_adapt = bdsf.process_image(os.path.join(fits_path,f'{fits_file}.fits'), thresh_pix=5.0, thresh_isl=4.0,adaptive_rms_box=True)
+img_adapt = bdsf.process_image(os.path.join(fits_path,f'{fits_file}.fits'), thresh_pix=5.0, thresh_isl=4.0,adaptive_rms_box=True,ncores=2)
 
 img.write_catalog(outfile=os.path.join(fits_path,f'{fits_file}_sources.reg'), format='ds9')
 img_adapt.write_catalog(outfile=os.path.join(fits_path, f'{fits_file}_sources_adapt.reg'), format='ds9')
