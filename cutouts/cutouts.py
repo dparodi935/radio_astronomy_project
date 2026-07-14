2b320a30d8a3da106cb1c86337a22a5ac7743e60
dparodi935 <dparodi935@gmail.com>
2026-07-14 18:27:02 +0100

    Added gaussian drawings to cutout

--- cutouts/cutouts.py
+++ cutouts/cutouts.py
@@ -2,9 +2,11 @@ from astropy.coordinates import SkyCoord
 from astropy.nddata import Cutout2D
 from astropy.table import Table
 from astropy.wcs import WCS
+from astropy.wcs.utils import proj_plane_pixel_scales
 from astropy.io import fits
 import os
 import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
 
 #create filepath of fits file
 fits_filename = "facet_19"
@@ -18,6 +20,7 @@ data = fits_file[0].data
 fits_file.close()
 
 coord_system = WCS(fits_file[0].header).celestial
+coord_scale = proj_plane_pixel_scales(coord_system)[1] #1 is chosen because pixels are square, and to avoid RA related issues
 
 #open PyBDSF catalogue and create Table
 catalogue_filename = f"{fits_filename}_sources"
@@ -26,8 +29,9 @@ catalogue = Table.read(catalogue_filepath, format="fits")
 
 ra_list = catalogue["RA"]
 dec_list = catalogue["DEC"]
+maj_list, min_list, pa_list = catalogue["Maj"], catalogue["Min"], catalogue["PA"]
 
-number = 10 #len(ra) - number of sources to cycle through
+number = 5 #len(ra) - number of sources to cycle through
 
 #cycles through every source and plots the cutout
 for i in range(number):
@@ -36,5 +40,14 @@ for i in range(number):
     size = 40
     cutout = Cutout2D(data, pixel_pos, size)
     
-    plt.imshow(cutout.data)
+    #set up image
+    fig, ax = plt.subplots()
+    
+    
+    ax.imshow(cutout.data, origin="lower")
+    
+    #draw ellipse
+    gaussian = Ellipse((size/2,size/2), min_list[i]/coord_scale, maj_list[i]/coord_scale, angle=pa_list[i], fill=False, lw=2, zorder=10)
+    ax.add_patch(gaussian)
+    
     plt.show()
