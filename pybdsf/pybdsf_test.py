8e3de4d9a364fbe5fc49a754b0368652a4d285bc
dparodi935 <dparodi935@gmail.com>
2026-07-15 11:50:04 +0100

    Changed to now overwrite previous catalog file

--- pybdsf/pybdsf_test.py
+++ pybdsf/pybdsf_test.py
@@ -8,9 +8,9 @@ fits_file = 'facet_19'
 #creates catalog. thresh_pix and isl are the S/N ratios for the peaks and surrounding island respectively.
 img = bdsf.process_image(os.path.join(fits_path,f'{fits_file}.fits'), thresh_pix=5.0, thresh_isl=4.0, ncores=2)
 
-
 #adaptive rms box means the box used to measure noise shrinks near bright sources to capture the noise artefacts
 img_adapt = bdsf.process_image(os.path.join(fits_path,f'{fits_file}.fits'), thresh_pix=5.0, thresh_isl=4.0,adaptive_rms_box=True,ncores=2)
 
-img.write_catalog(outfile=os.path.join(fits_path,f'{fits_file}_sources.fits'), format='fits')
-img_adapt.write_catalog(outfile=os.path.join(fits_path, f'{fits_file}_sources_adapt.fits'), format='fits')
+#write catalogue file. clobber=True: existing files overwritten
+img.write_catalog(outfile=os.path.join(fits_path,f'{fits_file}_sources.fits'), format='fits', clobber=True)
+img_adapt.write_catalog(outfile=os.path.join(fits_path, f'{fits_file}_sources_adapt.fits'), format='fits', clobber=True)
